f33d01b22ca14ba9ae98b7c072ab63b21ef7623d
unknown <momenmedhatsalem@gmail.com>
2024-04-11 17:35:18 +0200

    Collected files inside templatess

---
+++ static/js/js.js
@@ -0,0 +1,6 @@
+console.log("true outer");
+
+     window.localStorage.setItem("username","ahmed");
+    c = window.localStorage.getItem("username");
+    document.getElementById("username").innerHTML = c;
+ 
