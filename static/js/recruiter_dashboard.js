db2bde7017d32283b1d1c00637a28c6f9aba9ca2
Joseph-Sameh-0 <148119993+Joseph-Sameh-0@users.noreply.github.com>
2024-04-18 17:36:45 +0200

    .

--- static/js/recruiter_dashboard.js
+++ static/js/recruiter_dashboard.js
@@ -1,4 +1,3 @@
-
 // document.addEventListener("click", function (event) {
 //   if (event.target.classList.contains("J9_to_path")) {
 //     var J9_to_path = event.target.getAttribute("J9_to_path");
@@ -9,11 +8,56 @@
 //   }
 // });
 
-
-document.addEventListener("click", function(event) {
+document.addEventListener("click", function (event) {
   var J9_to_path = event.target.getAttribute("J9_to_path");
   if (J9_to_path !== null) {
-    document.getElementById("J9_main_container").setAttribute("J9_add_html", J9_to_path);
+    document
+      .getElementById("J9_main_container")
+      .setAttribute("J9_add_html", J9_to_path);
+
+    Array.from(
+      document.getElementsByClassName("J9_emp_dash_aside_menu_bar_button")
+    ).forEach(function (element) {
+      element.style.backgroundColor = "transparent";
+      element.style.color = "#000";
+    });
+
+    if (J9_to_path === "recruiter_dashboard_profile.html") {
+      document.getElementById("J9_emp_dash_right_side_body_title").innerHTML =
+        "Profile";
+      document.getElementsByClassName(
+        "J9_emp_dash_aside_menu_bar_button"
+      )[0].style = `background-color: #00c853;
+        color: #fff;`;
+    } else if (J9_to_path === "recruiter_dashboard_jobs.html") {
+      document.getElementById("J9_emp_dash_right_side_body_title").innerHTML =
+        "My Jobs";
+      document.getElementsByClassName(
+        "J9_emp_dash_aside_menu_bar_button"
+      )[1].style = `background-color: #00c853;
+        color: #fff;`;
+    } else if (J9_to_path === "recruiter_dashboard_post_job.html") {
+      document.getElementById("J9_emp_dash_right_side_body_title").innerHTML =
+        "Post a New Job";
+      document.getElementsByClassName(
+        "J9_emp_dash_aside_menu_bar_button"
+      )[2].style = `background-color: #00c853;
+        color: #fff;`;
+    } else if (J9_to_path === "recruiter_dashboard_saved_candidates.html") {
+      document.getElementById("J9_emp_dash_right_side_body_title").innerHTML =
+        "Saved Candidate";
+      document.getElementsByClassName(
+        "J9_emp_dash_aside_menu_bar_button"
+      )[3].style = `background-color: #00c853;
+        color: #fff;`;
+    } else if (J9_to_path === "recruiter_dashboard_settings.html") {
+      document.getElementById("J9_emp_dash_right_side_body_title").innerHTML =
+        "Account Settings";
+      document.getElementsByClassName(
+        "J9_emp_dash_aside_menu_bar_button"
+      )[4].style = `background-color: #00c853;
+        color: #fff;`;
+    }
     addHTML();
   }
 });
