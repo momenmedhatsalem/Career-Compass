603a020c271146bea535422413750f32d9795ece
jonathan <jonathanmokhles@gmail.com>
2024-04-19 23:22:14 +0200

    make the upload resume dynamic

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -7,3 +7,40 @@ function manageTabs(id) {
   document.getElementById(id).style.display = 'block';
 }
 
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var fileInput = document.getElementById('j0-uploadCV');
+  var uploadButton = document.querySelector('.j0-btn label');
+
+  // Function to update the display of uploaded files
+  function updateUploadedFilesDisplay(fileName) {
+    var uploadContainer = document.querySelector('.j0-input');
+    var uploadedFileDiv = document.createElement('div');
+    uploadedFileDiv.classList.add('j0-upload');
+    uploadedFileDiv.innerHTML = fileName + '<span class="j0-remove-btn"">x</span>';
+    uploadContainer.appendChild(uploadedFileDiv);
+
+  }
+
+  // Event listener for file input change
+  fileInput.addEventListener('change', function (event) {
+    var files = event.target.files;
+    for (var i = 0; i < files.length; i++) {
+      updateUploadedFilesDisplay(files[i].name);
+    }
+  });
+
+
+
+  // Event delegation to handle removal of uploaded files
+  document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('j0-remove-btn')) {
+      event.preventDefault();
+      event.target.closest('.j0-upload').remove();
+      updateUploadedFilesDisplay(files[i].name);
+    }
+  });
+
+});
