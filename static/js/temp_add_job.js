37ac64b2a2190f3d9e16489c96c20ea7702ac6f8
Mahmoud Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2024-04-20 23:00:43 +0200

    improved and finised all about search sorting and  result cards images ... DONE

--- static/js/temp_add_job.js
+++ static/js/temp_add_job.js
@@ -1,23 +1,24 @@
 
 class job {
 
-    constructor(title, exp, pay, country)
+    constructor(title, exp, pay, country, image)
     {
         this.title = title;
         this.exp = exp;
         this.pay = pay;
         this.country = country;
+        this.image = image;
     }
 };
 
-var job0 = new job("software developer", "3", 2000, "USA");
-var job1 = new job("software developer", "2", 3400, "Germany");
-var job2 = new job("software developer", "0", 3000, "France");
-var job3 = new job("front-end developer", "5", 1500, "Egypt");
-var job4 = new job("front-end developer", "3", 2000, "UAE");
-var job5 = new job("graphics designer", "6", 2300, "Spain");
-var job6 = new job("graphics designer", "2", 1900, "Russia");
-var job7 = new job("game developer", "3", 4000, "Russia");
+var job0 = new job("software developer", "3", 2000, "USA", "/media/data analyst.png");
+var job1 = new job("software developer", "2", 3400, "Germany", "/media/front end.jpg");
+var job2 = new job("software developer", "0", 3000, "France", "/media/Game dev.jpg");
+var job3 = new job("front-end developer", "5", 1500, "Egypt", "/media/graphics.jpg");
+var job4 = new job("front-end developer", "3", 2000, "UAE", "/media/sales manger.jpg");
+var job5 = new job("graphics designer", "6", 2300, "Spain", "/media/software.jpg");
+var job6 = new job("graphics designer", "2", 1900, "Russia", "/media/ui ux.jpg");
+var job7 = new job("game developer", "3", 4000, "Russia", "/media/front end.jpg");
 
 var job_array = [];
 
