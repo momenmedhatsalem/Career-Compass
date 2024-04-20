93b5d6a8d84dec0e4bd63bca4f0a9dbf43bda6c7
Joseph-Sameh-0 <148119993+Joseph-Sameh-0@users.noreply.github.com>
2024-04-21 01:29:51 +0200

    .

--- static/js/recruiter_dashboard_jobs.js
+++ static/js/recruiter_dashboard_jobs.js
@@ -1,3 +1,46 @@
+var my_jobs = JSON.parse(localStorage.getItem("my_jobs"));
+
+let my_jobs_table = document.getElementById(
+  "J9_emp_dash_jobs_right_side_body_emp_jobs_table_body"
+);
+
+my_jobs.forEach((element) => {
+  let row = document.createElement("tr");
+  row.className = "J9_emp_dash_jobs_right_side_body_emp_jobs_table_body_row";
+  let html = `
+  <td class="J9_emp_dash_jobs_right_side_body_emp_jobs_table_body_row_title">
+    <div>${element.details.title}</div>
+    <div>${element.details.type} . ${element.address_and_location.country}</div>
+    </td>
+    <td>${element.details.creation_date}</td>
+    <td>${Math.floor(Math.random() * (300 - 10 + 1)) + 10} Applications</td>
+    <td>
+    <div class="${ element.details.status === "open" ? "J9_open" : "J9_closed" } J9_emp_dash_jobs_right_side_body_emp_jobs_table_body_row_status">
+    ${element.details.status}
+    </div>
+  </td>
+  <td>
+    <div class="J9_emp_dash_jobs_right_side_body_emp_jobs_table_body_row_action_dots">
+      <button class="J9_emp_dash_jobs_right_side_body_emp_jobs_table_body_row_action_button">
+        <span></span>
+        </button>
+        <ul class="J9_emp_dash_jobs_right_side_body_emp_jobs_table_body_row_action_ul">
+        <li><a href="#"><img src="../../media/icons/eye.svg">View</a>
+        </li>
+        <li><a href="#"><img src="../../media/icons/edit.svg">Edit</a>
+        </li>
+        <li><a href="#"><img src="../../media/icons/delete.svg">Delete</a></li>
+      </ul>
+    </div>
+  </td>`;
+  row.innerHTML = html;
+
+  my_jobs_table.appendChild(row);
+});
+
+
+/*⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻*/
+
 document.addEventListener("click", function (event) {
   if (
     !event.target.matches(
