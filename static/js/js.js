83240bd479abffa62c85a44ad05115630f1091d4
philokaram <philokrm@gmail.com>
2024-04-21 14:51:55 +0200

    14

--- static/js/js.js
+++
@@ -1,6 +0,0 @@
-console.log("true outer");
-
-     window.localStorage.setItem("username","ahmed");
-    c = window.localStorage.getItem("username");
-    document.getElementById("username").innerHTML = c;
- 
