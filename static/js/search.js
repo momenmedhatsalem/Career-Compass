e2dcf5be035a0c795f483d0f06059f969d3c9fe7
Mahmoud Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2024-04-29 18:53:50 +0300

    added searching by years of experince and country in search page

--- static/js/search.js
+++ static/js/search.js
@@ -12,8 +12,30 @@ var images = [
 ];
 
 var matches = [];
-function search() {
+search_by = "";
+old_option = "title";
 
+function set_search_by(opt = "title")
+{
+    document.getElementById(old_option).style.backgroundColor = "#ddd";
+    document.getElementById(old_option).style.color = "black";
+
+    search_by = opt;
+    old_option = search_by;
+
+    matches = [];
+
+    document.getElementById("result").style.display = "none";
+    document.getElementById("result-grid").style.display = "none";
+
+    document.getElementById(opt).style.backgroundColor = "rgb(0, 136, 255)";
+    document.getElementById(opt).style.color = "white";
+
+}
+
+set_search_by();
+
+function search() {
 
     // check if the search bar is empty or not
     if (document.getElementById("search_text").value == "")
@@ -29,29 +51,64 @@ function search() {
     // and "matches" to store atching jobs based on "sub"
 
     var sub = document.getElementById("search_text").value;
-
-
-
     let jobs = JSON.parse(localStorage.getItem("jobs"));
 
 
     // make "sub" lowerCase to standardize our search
-    sub = sub.toLowerCase();
+    
+    
+    // sub = sub.toLowerCase();
+
 
     // test: --> (jobs arrived safely from localstorage)
     // console.log(jobs[2].details.MaxSalary);
 
     // loop through "jobs" and push any "job" having a ".title" including a substring of "sub"
     matches = [];
-    for (let job of jobs) {
-        console.log(job)
-        // Check if job has 'details' property and 'title' property within it
-        if (
-            // job.details &&
-            // job.details.title &&
-            job.details.title.toLowerCase().includes(sub)
-        ) {
-            matches.push(job);
+
+    console.log(search_by);
+
+    if(search_by == "title")
+    {
+
+        for (let job of jobs)
+        {
+            // console.log(job)
+            // Check if job has 'details' property and 'title' property within it
+            if (job.details.title.toLowerCase().includes(sub.toLowerCase())) 
+            {
+                matches.push(job);
+            }
+        }
+    }
+    else if(search_by == "years")
+    {
+        
+        for (let job of jobs)
+        {
+            // console.log(job)
+            // Check if job has 'details' property and 'title' property within it
+            if (job.details.years_of_experience <= sub) 
+            {
+                matches.push(job);
+            }
+        }
+
+        if(isNaN(sub))
+        {
+            matches = [];
+        }
+    }
+    else if(search_by == "country")
+    {
+        for (let job of jobs)
+        {
+            //console.log(job)
+            // Check if job has 'details' property and 'title' property within it
+            if (job.address_and_location.country.toLowerCase().includes(sub.toLowerCase()))
+            {
+                matches.push(job);
+            }
         }
     }
 
@@ -112,7 +169,7 @@ function showmatches() {
             match.details.title +
             "</h4>" +
             "<p>" +
-            match.details.MaxSalary + "$ | " + match.details.years_of_experience + " Exp. years needed | " + match.address_and_location.country +
+            match.details.MaxSalary + "$ | " + match.details.years_of_experience + " Exp. years | " + match.address_and_location.country +
             "</p>" +
             // "<p>" +
             // match.address_and_location.country +
