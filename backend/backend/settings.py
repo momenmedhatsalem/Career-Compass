b4e2b4efd379ee74ec7d2eec10c9958aa4d9e8cf
momenmedhatsalem <momenmedhat10@hotmail.com>
2024-05-03 07:57:17 +0300

    Connceted Django to our Index page

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -117,7 +117,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATICFILES_DIRS = [
+    BASE_DIR.parent / "static",
+    BASE_DIR.parent / 'media',
+    
+]
 STATIC_URL = "static/"
 
 # Default primary key field type
