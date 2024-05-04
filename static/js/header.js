9e01714c1b2d8b3202499806033d4f8af951ec1a
Joseph-Sameh-0 <148119993+Joseph-Sameh-0@users.noreply.github.com>
2024-05-04 18:21:28 +0300

    .

--- static/js/header.js
+++ static/js/header.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   var userList = JSON.parse(localStorage.getItem("userList")) || [];
-  if (!userList.some((user) => user.loggedin)) {
+  if (userList.some((user) => user.loggedin)) {
     document.getElementById("J9_login_signup").style.display = "none";
   }
 });
