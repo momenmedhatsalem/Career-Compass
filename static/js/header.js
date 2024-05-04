18acd93cc2f1f8dcfb95e04e00e11712d4d97fbb
Joseph-Sameh-0 <148119993+Joseph-Sameh-0@users.noreply.github.com>
2024-05-04 18:48:36 +0300

    .

--- static/js/header.js
+++ static/js/header.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   var userList = JSON.parse(localStorage.getItem("userList")) || [];
-  if (userList.some((user) => user.loggedin)) {
-    document.getElementById("J9_login_signup").style.display = "none";
+
+  if (userList.some((user) => user.loggedin && user.type != "recruiter")) {
+    document.getElementById("J9_login_signup_container").style.display = "none";
+  } else {
+    if (userList.some((user) => user.loggedin)) {
+      document.getElementById("J9_login_signup").style.display = "none";
+      if (user.type != "recruiter") {
+        document.getElementById("J9_login_signup_post").style.display = "none";
+      }
+    }
   }
 });
