65fbbbbd465d8f2b91c05ef856f0ae1826fac385
philokaram <philokrm@gmail.com>
2024-05-13 16:05:25 +0300

    save job done

--- backend/CareerApp/views.py
+++ backend/CareerApp/views.py
@@ -12,7 +12,6 @@ from django.views.decorators.http import require_http_methods
 def index(request):
     return render(request, "index.html")
 
-
 @login_required
 def profile(request):
     user = request.user
@@ -328,7 +327,18 @@ def saveRecSettings(request):
 
 def viewJob(request, job_id, recruiter_username):
     job = get_object_or_404(Job, pk=job_id, recruiter__user__username=recruiter_username)
-    return render(request, 'Job_Details.html', {'job': job})
+    if request.user.is_authenticated :
+        if request.user.is_applicant: 
+            all_saved_jobs = SavedJob.objects.all()
+            saved = False
+            for j in all_saved_jobs:
+                if j.job.job_id == job_id and j.applicant.user.username == request.user.username :
+                    saved = True
+                    break
+    if saved :
+        return render(request, 'Job_Details.html', {'job': job,'saved':1})
+    else:
+        return render(request, 'Job_Details.html', {'job': job})
 
 def editJob(request, job_id):
     pass
