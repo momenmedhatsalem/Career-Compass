351431ac64c3422bf0154cbff6ef05995c00520b
jonathan <jonathanmokhles@gmail.com>
2024-05-13 16:38:06 +0300

    ..

--- backend/CareerApp/views.py
+++ backend/CareerApp/views.py
@@ -18,7 +18,6 @@ states = ["Cairo", "Alexandria", "Giza", "Luxor", "Aswan", "Sharm El Sheikh", "H
 def index(request):
     return render(request, "index.html")
 
-
 @login_required
 def profile(request):
     user = request.user
@@ -325,12 +324,24 @@ def saveRecSettings(request):
 
 def viewJob(request, job_id, recruiter_username):
     job = get_object_or_404(Job, pk=job_id, recruiter__user__username=recruiter_username)
-    return render(request, 'Job_Details.html', {'job': job})
+    saved = False
+    if request.user.is_authenticated :
+        if request.user.is_applicant: 
+            all_saved_jobs = SavedJob.objects.all()
+            for j in all_saved_jobs:
+                if j.job.job_id == job_id and j.applicant.user.username == request.user.username :
+                    saved = True
+                    break
+    if saved :
+        return render(request, 'Job_Details.html', {'job': job,'saved':1})
+    else:
+        return render(request, 'Job_Details.html', {'job': job,'saved':0})
 
 def editJob(request, job_id, recruiter_username):
     job = get_object_or_404(Job, pk=job_id, recruiter__user__username=recruiter_username)
     if request.method == 'POST':
         # Handle the job editing form submission
+        job.job_id = request.POST.get("job-id")
         job.title = request.POST.get("job-title")
         job.company_name = request.POST.get("company-name")
         job.years_of_experience = request.POST.get("years-of-experience")
