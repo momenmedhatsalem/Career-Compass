ef9a562499795557dd2c915d78e19633c6fc4236
momenmedhatsalem <momenmedhat10@hotmail.com>
2024-05-18 16:36:03 +0300

    fixed the job model and created a new database

--- backend/CareerApp/models.py
+++ backend/CareerApp/models.py
@@ -10,8 +10,8 @@ class JobCategory(models.Model):
         return self.name
 
 class Job(models.Model):
-    job_id = models.AutoField(primary_key=True)
-    id = models.IntegerField()
+    id = models.AutoField(primary_key=True)
+    job_id = models.IntegerField(default=None, blank=True, null=True)
     recruiter = models.ForeignKey(Recruiter, on_delete=models.CASCADE, default=None)
     title = models.CharField(max_length=100, default=None)
     company_name = models.CharField(max_length=100, default=None)
@@ -36,7 +36,7 @@ class Job(models.Model):
     def __str__(self):
         return self.title
     class Meta:
-        unique_together = ('id', 'recruiter',)
+        unique_together = ('job_id', 'recruiter',)
 
 
 class SavedJob(models.Model):
