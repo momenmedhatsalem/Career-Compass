9c8ff7590f56e34a762a9dda30b8836423415609
jonathan <jonathanmokhles@gmail.com>
2024-05-22 19:12:48 +0300

    ........

--- backend/UserApp/views.py
+++ backend/UserApp/views.py
@@ -171,7 +171,7 @@ from .models import Applicant, Experience
 def save_experience(request):
     if request.method == 'POST':
         applicant_id = request.user.id
-        experience_id = request.POST.get('experience_id')  # Get the experience ID from the form
+        # experience_id = request.POST.get('experience_id')  # Get the experience ID from the form
         title = request.POST.get('title')
         company = request.POST.get('Company')
         start_date = request.POST.get('startDate')
@@ -183,9 +183,10 @@ def save_experience(request):
         except Applicant.DoesNotExist:
             # Handle the case where the applicant does not exist
             return redirect('error_page')  # Replace 'error_page' with your actual error page
+        experience_id= Experience.objects.filter(applicant=applicant_id , title = title)
         if experience_id:
             # Update the existing experience
-            experience = get_object_or_404(Experience, id=experience_id, applicant=applicant)
+            # experience = get_object_or_404(Experience, id=experience_id, applicant=applicant)
             experience.title = title
             experience.Company = company
             experience.startDate = start_date
